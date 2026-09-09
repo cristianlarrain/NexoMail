09f0ca987dc4e1dc812c9a464dba757a27c17adb
cristianlarrain <83875789+cristianlarrain@users.noreply.github.com>
2026-09-08 22:21:17 -0300

    feat: use Nexi action plan router

--- src/frontend/src/router.tsx
+++ src/frontend/src/router.tsx
@@ -10,7 +10,7 @@ import { ControlCenterPage } from './pages/ControlCenterPage'
 import { InboxPage } from './pages/InboxPage'
 import { LandingPage } from './pages/LandingPage'
 import { MessageRoute } from './pages/MessageRoute'
-import { NexiSearchActionPage } from './pages/NexiSearchActionPage'
+import { NexiSearchActionRoute } from './pages/NexiSearchActionRoute'
 import { ProfilePage } from './pages/ProfilePage'
 import { SearchPage } from './pages/SearchPage'
 
@@ -22,7 +22,7 @@ export const router = createBrowserRouter([
     children: [
       { path: '/inbox', element: <InboxPage /> },
       { path: '/search', element: <SearchPage /> },
-      { path: '/search-action', element: <NexiSearchActionPage /> },
+      { path: '/search-action', element: <NexiSearchActionRoute /> },
       { path: '/control-center', element: <ControlCenterPage /> },
       { path: '/account/:accountId', element: <InboxPage /> },
       { path: '/archive', element: <InboxPage folder="archive" /> },
